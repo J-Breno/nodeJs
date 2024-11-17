011b9ba55d50ad5c2f8ac311f551ee3ee29f8501
J-Breno <joao.breno85@hotmail.com>
2024-11-17 08:00:19 -0300

    Aprendendo sobre methodHttp e url

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,29 @@
 import http from "node:http";
 
+// GET, POST, PUT, PATCH, DELETE
+// GET = Buscar recurso do backend
+// POST = Sempre para criar uma recurso do backend
+// PUT = Atualizar um recurso no backend
+// PATCH = Atualizar uma informação única/específica no backend => PUT atuliza mais de uma coisa ao mesmo tempo, PATCH atualização específica
+// DELETE = deletar um recurso no back-end
+
+// GET /users => Buscando usuários no back-end
+// POST /user => Criando um usuário no back-end
+
+
 const server = http.createServer((req, res) => {
+
+    const { method, url } = req;
+
+    if(method === "GET" && url === '/users') {
+        // Early return -> se bater no return nada abaixo vai ser executado
+        return res.end("Listagem de usuários")
+    }
+
+    if(method === 'POST' && url === "/users") {
+        return res.end("Criação de usuários")
+    }
+
     return res.end('Hello, Ignite!');
 });
 
