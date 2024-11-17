4820e56e43fc0934644e728e8f01fa48846a0074
J-Breno <joao.breno85@hotmail.com>
2024-11-17 09:14:22 -0300

    Aprendendo sobre os headers

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -10,6 +10,15 @@ import http from "node:http";
 // GET /users => Buscando usuários no back-end
 // POST /user => Criando um usuário no back-end
 
+// Stateful != Stateles
+
+// Stateful ela sempre vai ter um informação guardada em memória, ou seja, a aplicação depende da memória, das informações salvas em memória para continuar funcionando, apatir que ela for derrubada ela se perde
+
+// Stateles ela não salva em memória, ela salva em um banco de dados, ou seja, se pararmos essa aplicação ela continua igual
+
+const users = [];
+
+
 
 const server = http.createServer((req, res) => {
 
@@ -17,10 +26,15 @@ const server = http.createServer((req, res) => {
 
     if(method === "GET" && url === '/users') {
         // Early return -> se bater no return nada abaixo vai ser executado
-        return res.end("Listagem de usuários")
+        // Content-type diz qual é o tipo de conteúdo que está sendo usado
+        // applicaton/json é para informa que o tipo de conteúdo é um JSON
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
     }
 
     if(method === 'POST' && url === "/users") {
+        users.push({id: 1, name: 'Breno', email: "breno@gmail.com"})
         return res.end("Criação de usuários")
     }
 
